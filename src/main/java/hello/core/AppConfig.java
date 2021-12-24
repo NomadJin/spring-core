8cf4664b308931d68fb72197b50fe135e05186cd
jin85-kim <jin85.kim@sk.com>
2021-12-24 16:14:39 +0900

    first commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,6 +10,8 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Scanner;
+
 @Configuration
 public class AppConfig {
 
